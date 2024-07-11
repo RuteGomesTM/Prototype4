4b9414044b5901fcb917536bdf40775c23f1a03f
RuteGomesTM <164046128+RuteGomesTM@users.noreply.github.com>
2024-07-11 22:35:14 +0100

    Challenge 4 - Soccer Scripting - Fim

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,6 +14,9 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+    private float turboBoost = 10f;
+
+    public ParticleSystem turbosSmoke;
     
     void Start()
     {
@@ -30,6 +33,12 @@ public class PlayerControllerX : MonoBehaviour
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * turboBoost, ForceMode.Impulse);
+            turbosSmoke.Play();
+        }
+
     }
 
     // If Player collides with powerup, activate powerup
@@ -37,6 +46,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Powerup"))
         {
+            StartCoroutine(PowerupCooldown());
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
@@ -57,7 +67,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
